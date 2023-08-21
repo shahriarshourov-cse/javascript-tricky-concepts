765282c599cac9a446fcf276c0749d9ac2e246cc
Shahriar Shourov <shahriarshourov@github.com>
2023-08-21 11:50:31 +0600

    first commit

---
+++ 01-function-hoisting.js
@@ -0,0 +1,8 @@
+/* 
+Scope -> variables are only accessible some places.
+Hoisting -> Code order does not always dictate
+“execution order”
+3 types of scope : local scope,global scope, block scope
+  use "var" when declare variable in local & global scope
+  
+*/
